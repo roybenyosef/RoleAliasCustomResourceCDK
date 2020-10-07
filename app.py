1567f2f118a5e5ab344e54f9cf4895681fc47788
Roy Ben Yosef <roy.benyosef@cyberark.com>
2020-10-07 20:53:18 +0300

    remove comments

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ from role_alias_custom_resource.role_alias_custom_resource_stack import RoleAlia
 
 app = core.App()
 
-# env_neo = core.Environment(account="450676674096", region="eu-west-1")
-# env_everest = core.Environment(account="450676674096", region="eu-west-1")
-
-# RoleAliasCustomResourceStack(app, "role-alias-custom-resource-stack", env=env_neo)
 RoleAliasCustomResourceStack(app, "role-alias-custom-resource-stack")
 
 app.synth()
